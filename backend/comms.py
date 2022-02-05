21bece4500cd783c83b42902733f83e87e97a16b
Dol3v <dol12dab@gmail.com>
2022-02-05 20:06:19 +0200

    changed time format and added server-side ecdh

--- backend/comms.py
+++ backend/comms.py
@@ -1,15 +1,22 @@
 """Socket-Communcation API"""
 
-import struct
 import hmac
-import time
 import logging
 
 from datetime import datetime
 from socket import socket
 from enum import IntEnum
 from typing import NamedTuple
-from consts import HMAC_SIZE, HEADER_SIZE
+from cryptography.hazmat.primitives.asymmetric.ec import (
+    generate_private_key,
+    derive_private_key,
+    ECDH,
+    EllipticCurvePublicKey,
+)
+from cryptography.hazmat.primitives.serialization import Encoding, PublicFormat
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+from cryptography.hazmat.primitives.hashes import SHA256
+from backend.consts import COMPRESSED_POINT_SIZE, ELLIPTIC_CURVE, HMAC_SIZE, HEADER_SIZE
 
 
 class PacketID(IntEnum):
@@ -57,7 +64,7 @@ def create_packet(content: bytes, type: PacketID, key: bytes) -> bytes:
     bytes
         the packet's data
     """
-    encoded_time = struct.pack("f", time.mktime(datetime.now().timetuple()))
+    encoded_time = int(datetime.now().timestamp() * 1000).to_bytes(8, "big")
     header = (
         int(type).to_bytes(1, "big") + len(content).to_bytes(2, "big") + encoded_time
     )
@@ -67,7 +74,7 @@ def create_packet(content: bytes, type: PacketID, key: bytes) -> bytes:
 
 def unpack_packet(conn: socket, key: bytes) -> PacketInfo:
     """Validates the packet data and returns the packet's content.
-    Raises `ArgumentError` if the packet's given HMAC doesn't align with its actual HMAC.
+    Raises `ValueError` if the packet's given HMAC doesn't align with its actual HMAC.
 
     Parameters
     ----------
@@ -78,8 +85,8 @@ def unpack_packet(conn: socket, key: bytes) -> PacketInfo:
 
     Returns
     -------
-    bytes
-        the packet's contents
+    PacketInfo
+        the packet's contents and metadata
     """
     # parsing header
     header = conn.recv(HEADER_SIZE)
@@ -88,7 +95,7 @@ def unpack_packet(conn: socket, key: bytes) -> PacketInfo:
         type, content_length, time_stamp = (
             PacketID(header[0]),
             int.from_bytes(header[1:3], "big"),
-            datetime.fromtimestamp(struct.unpack("f", header[3:])[0]),
+            datetime.fromtimestamp(int.from_bytes(header[3:], "big") / 1000),
         )
     except Exception:
         logging.warning(f"Invalid header was given to socket {conn}", exc_info=True)
@@ -99,5 +106,33 @@ def unpack_packet(conn: socket, key: bytes) -> PacketInfo:
         logging.critical(
             f"HMACs doesn't match in message given to {conn}: {content=},{time_stamp=},{content_length=},{type=},{msg_hmac=}"
         )
-        return
+        raise ValueError("message HMAC doesn't match contents")
     return PacketInfo(type, time_stamp, content)
+
+
+def get_shared_key(conn: socket) -> bytes:
+    """Uses ECDH (aka crypto magic) to update a shared key.
+
+    Parameters
+    ----------
+    conn : socket
+        connection with client
+
+    Returns
+    ----------
+    bytes
+        The derived shared key
+    """
+    private_key = generate_private_key(ELLIPTIC_CURVE)  # get private key
+    conn.send(
+        private_key.public_key().public_bytes(
+            Encoding.X962, PublicFormat.CompressedPoint
+        )
+    )
+    peer_public_key = EllipticCurvePublicKey.from_encoded_point(
+        curve=ELLIPTIC_CURVE, data=conn.recv(COMPRESSED_POINT_SIZE)
+    )
+    shared = private_key.exchange(ECDH, peer_public_key)
+    return HKDF(
+        algorithm=SHA256(), length=32, salt=None, info=b"handshake data"
+    ).derive(shared)
--- backend/consts.py
+++ backend/consts.py
@@ -2,9 +2,11 @@
 
 SERVER_IPv6 = "smth"
 
+from cryptography.hazmat.primitives.asymmetric.ec import SECP384R1
+
 
 # Packet structure consts, sizes in bytes
-SHARED_KEY_SIZE = 48
+SHARED_KEY_SIZE = 32
 HMAC_SIZE = 16 
 TYPE_SIZE = 1
 CONTENT_LENGTH_SIZE = 2
@@ -12,3 +14,8 @@ TIMESTAMP_SIZE = 4
 
 
 HEADER_SIZE = HMAC_SIZE + TYPE_SIZE + CONTENT_LENGTH_SIZE + TIMESTAMP_SIZE
+
+
+# ECDH Consts
+COMPRESSED_POINT_SIZE = 49
+ELLIPTIC_CURVE = SECP384R1()
